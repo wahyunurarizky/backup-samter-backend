f3a58177a2fcae8d434866808713a83bfdcdb267
jabbar-hafizh <jabbarworld1@gmail.com>
2021-06-21 11:48:21 +0700

    crud bank

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -4,5 +4,5 @@ const base = require('./baseController');
 exports.create = base.createOne(Bank, 'number_account', 'name');
 exports.getAll = base.getAll(Bank);
 exports.get = base.getOne(Bank);
-exports.update = base.updateOne(Bank);
+exports.update = base.updateOne(Bank, 'number_account', 'name');
 exports.delete = base.deleteOne(Bank);
