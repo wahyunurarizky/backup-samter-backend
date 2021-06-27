169105ad17beabfae90fa0e7323172052225053a
jabbar-hafizh <jabbarworld1@gmail.com>
2021-06-27 17:04:53 +0700

    finish complaint

--- models/complaintModel.js
+++ models/complaintModel.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
 const complaintSchema = new mongoose.Schema({
+  time: Date,
+  name: {
+    type: String,
+    required: true,
+  },
   nik: {
     type: String,
     trim: true,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  kelurahan: {
+    type: String,
+    required: true,
+  },
+  kecamatan: {
+    type: String,
     required: true,
   },
   phone: {
@@ -20,10 +37,6 @@ const complaintSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  loc: {
-    type: String,
-    required: true,
-  },
   status: {
     type: String,
     default: 'terkirim',
@@ -33,6 +46,36 @@ const complaintSchema = new mongoose.Schema({
   solution: {
     type: String,
   },
+});
+
+complaintSchema.post('save', function (next) {
+  this._doc.time = this.time.toLocaleString('id-ID', {
+    hour12: false,
+  });
+});
+
+complaintSchema.post(/^find/, (result) => {
+  console.log(!this.time);
+  if (Array.isArray(result)) {
+    result.forEach((e) => {
+      if (e.time)
+        e._doc.time = e.time.toLocaleString('id-ID', {
+          hour12: false,
+        });
+      if (e.arrival_time)
+        e._doc.arrival_time_local = e.arrival_time.toLocaleString('id-ID', {
+          hour12: false,
+        });
+    });
+  } else {
+    if (result.time)
+      result._doc.time = result.time.toLocaleString('id-ID', { hour12: false });
+    if (result.arrival_time)
+      result._doc.arrival_time_local = result.arrival_time.toLocaleString(
+        'id-ID',
+        { hour12: false }
+      );
+  }
 });
 
 const Complaint = mongoose.model('Complaint', complaintSchema);
