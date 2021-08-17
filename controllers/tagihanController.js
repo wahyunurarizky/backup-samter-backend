6fc5151ddcb56bc85c63bc398c52da8f68718d03
Wahyu Nur Arizky <arizky.wna@gmail.com>
2021-08-17 17:43:11 +0700

    asddas

--- controllers/tagihanController.js
+++ controllers/tagihanController.js
@@ -557,26 +557,39 @@ exports.notificationCheckout = async (req, res, next) => {
       serverKey: 'SB-Mid-server-kyQCjeU8EZJQYb_NDoLmt19q',
       clientKey: 'SB-Mid-client-TnDd7DdTmVrqeia4',
     });
-    console.log('ini req body', req.body);
-    const mockNotificationJson = {
-      currency: 'IDR',
-      fraud_status: 'accept',
-      gross_amount: '24145.00',
-      order_id: 'test-transaction-321',
-      payment_type: 'bank_transfer',
-      status_code: '201',
-      status_message: 'Success, Bank Transfer transaction is created',
-      transaction_id: '6ee793df-9b1d-4343-8eda-cc9663b4222f',
-      transaction_status: 'pending',
-      transaction_time: '2018-10-24 15:34:33',
-      va_numbers: [{ bank: 'bca', va_number: '490526303019299' }],
-    };
 
-    const statusResponse = await apiClient.transaction.notification(
-      mockNotificationJson
+    const statusResponse = await apiClient.transaction.notification(req.body);
+    console.log('statusRespones', statusResponse);
+    const orderId = statusResponse.order_id;
+    const transactionStatus = statusResponse.transaction_status;
+    const fraudStatus = statusResponse.fraud_status;
+
+    console.log(
+      `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`
     );
 
-    console.log(statusResponse);
+    // Sample transactionStatus handling logic
+
+    if (transactionStatus === 'capture') {
+      // capture only applies to card transaction, which you need to check for the fraudStatus
+      if (fraudStatus === 'challenge') {
+        // TODO set transaction status on your databaase to 'challenge'
+      } else if (fraudStatus === 'accept') {
+        // TODO set transaction status on your databaase to 'success'
+      }
+    } else if (transactionStatus === 'settlement') {
+      // TODO set transaction status on your databaase to 'success'
+    } else if (transactionStatus === 'deny') {
+      // TODO you can ignore 'deny', because most of the time it allows payment retries
+      // and later can become success
+    } else if (
+      transactionStatus === 'cancel' ||
+      transactionStatus === 'expire'
+    ) {
+      // TODO set transaction status on your databaase to 'failure'
+    } else if (transactionStatus === 'pending') {
+      // TODO set transaction status on your databaase to 'pending' / waiting payment
+    }
     // .then((statusResponse) => {
     //   const orderId = statusResponse.order_id;
     //   const transactionStatus = statusResponse.transaction_status;
